7728cdd9d3d24ea300b2ba304ce0da8754a69742
abssafi <abs2002.safi@gmail.com>
2025-07-11 16:21:45 +0500

    second commit

--- wishbone/sv/wb_master_monitor.sv
+++ wishbone/sv/wb_master_monitor.sv
@@ -1,5 +1,16 @@
 class wb_master_monitor extends superClass;
-    function new();
-        
-    endfunction //new()
-endclass //wb_master_monitor extends superClass
+    `uvm_component_utils(wb_master_monitor)
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction : new
+
+    virtual task run_phase(uvm_phase phase);
+        `uvm_info(get_type_name(), $sfromatf("Executing Monitor Run Phase!"))
+    endtask : run_phase
+
+    function void start_of_simulation_phase(uvm_phase phase);
+        super.start_of_simulation_phase(phase);
+        `uvm_info(get_type_name(), "Running Simulation Monitor!", UVM_HIGH)
+    endfunction : start_of_simulation_phase
+endclass : wb_master_monitor
